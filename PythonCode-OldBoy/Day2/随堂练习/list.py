f76292b78c26ecad6eea66d54441b920dc140dbc
曾垂鑫 <zengchuixin@126.com>
2017-07-13 23:00:11 +0800

    更新Day2随堂了练习-列表

--- PythonCode-OldBoy/Day2/随堂练习/list.py
+++ PythonCode-OldBoy/Day2/随堂练习/list.py
@@ -26,6 +26,66 @@ names.append("zengchuixin5")
 print(names)
 
 # 追加的值放到随意的位置
+# 放在zengchuixin2的前面，就写zengchuixin2所在的位置
+names.insert(1, "zengchuixina")
+names.insert(3, "zengchuixinb")
+print(names)
+
+# 修改，替换zengchuixin2为zengchuixinc
+names[2] = "zengchuixinc"
+print(names)
+
+# 删除有三种方法
+
+names.remove("zengchuixinc")
+print(names)
+
+del names[2]
+print(names)
+
+# 删掉最后一个
+names.pop() # 如果后面加上下标，例如names.pop(2) 就相当于 del names[2]
+print(names)
+
+# 在列表中找出某个人，假如列表人非常多，怎么快速找
+# 知道具体的名称，通过把名字传递给列表，找到该名字所在的位置
+print(names.index("zengchuixina"))
+# 找到值所在的序号之后，可以把这个值输出打印
+print(names[names.index("zengchuixina")])
+
+# 清空列表
+names.clear()
+print(names)
+
+# 列表里可以有重名的人
+names = ["dengchuixin", "zengchuixin","aengchuixin2", "zengchuixin3", "zengchuixin4"]
+print(names)
+# 统计列表有多少重复的人
+print(names.count("zengchuixin"))
+
+# reverse反转列表
+names.reverse()
+print(names)
+
+# sort方法用于排序，默认按照特殊符号、数字、大写、小写的顺序进行排序，按照ASCII吗的方式
+names.sort()
+print(names)
+
+# 扩展列表,将另外一个列表合并到当前列表
+names2 = [1,2,3,4]
+names.extend(names2)
+print(names)
+print(names2)
+
+# 删除列表
+del names2
+print(names2)
+
+
+
+
+
+
 
 
 
