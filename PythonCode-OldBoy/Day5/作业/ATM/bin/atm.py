c6b0bb4ca819bfaa1a9208771650e0ea73a628b8
Chuixin Zeng(曾垂鑫_IT) <zengchuixin@126.com>
2017-08-24 17:22:48 +0800

    更新Day5作业

--- PythonCode-OldBoy/Day5/作业/ATM/bin/atm.py
+++ PythonCode-OldBoy/Day5/作业/ATM/bin/atm.py
@@ -1,3 +1,13 @@
 # -*- coding:utf-8 -*-
 
 # Author:Chuixin Zeng
+
+import os
+import sys
+base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+print(base_dir)
+sys.path.append(base_dir)
+
+from core import main
+if __name__ == '__main__':
+    main.run()
--- PythonCode-OldBoy/Day5/作业/ATM/db/account_sample.py
+++ PythonCode-OldBoy/Day5/作业/ATM/db/account_sample.py
@@ -1,3 +1,17 @@
 # -*- coding:utf-8 -*-
 
 # Author:Chuixin Zeng
+
+import json
+acc_dic = {
+    'id': 1234,
+    'password': 'abc',
+    'credit': 15000,
+    'balance': 15000,
+    'enroll_date': '2016-01-02',
+    'expire_date': '2021-01-01',
+    'pay_day': 22,
+    'status': 0 # 0 = normal, 1 = locked, 2 = disabled
+}
+
+print(json.dumps(acc_dic))
