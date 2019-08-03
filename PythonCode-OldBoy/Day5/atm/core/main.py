764bfc517078965e6efbe8731d5b6efcd548b2bf
zengchuixin <zengchuixin@126.com>
2019-08-03 10:36:01 +0800

    update

--- PythonCode-OldBoy/Day5/atm/core/main.py
+++ PythonCode-OldBoy/Day5/atm/core/main.py
@@ -8,7 +8,7 @@
 import os
 import sys
 base_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-#print(base_dir)
+print(base_dir)
 sys.path.append(base_dir)
 
 from core import auth
